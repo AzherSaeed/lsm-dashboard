6437c461ca65ecae8d589e8ab012748d1c93f77b
@azher <azher@renesistech.com>
2023-09-12 20:30:49 +0500

    envoirment variable changed

--- src/environment.js
+++ src/environment.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
-let BASE_URL = 'https://lsm-back-end.herokuapp.com/api/';
-if (process.env.REACT_APP_ENV === 'development') {
-	BASE_URL = 'https://lsm-back-end.herokuapp.com/api/';
+let BASE_URL = "https://lsm-backend-production.up.railway.app/api/";
+if (process.env.REACT_APP_ENV === "development") {
+  BASE_URL = "https://lsm-backend-production.up.railway.app/api/";
 }
 
-if (process.env.REACT_APP_ENV === 'staging') {
-	BASE_URL = 'http://staging.project.com';
+if (process.env.REACT_APP_ENV === "staging") {
+  BASE_URL = "http://staging.project.com";
 }
 
-if (process.env.REACT_APP_ENV === 'production') {
-	BASE_URL = 'http://prod.project.com';
+if (process.env.REACT_APP_ENV === "production") {
+  BASE_URL = "http://prod.project.com";
 }
 
-export {BASE_URL};
+export { BASE_URL };
